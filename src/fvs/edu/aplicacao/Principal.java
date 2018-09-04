302e5547a2c1bd410f3f6c2ee83eb21b90c91f00
thalislima <thalislima009@gmail.com>
2018-09-04 20:15:51 -0300

    aulaJPA2

--- src/fvs/edu/aplicacao/Principal.java
+++ src/fvs/edu/aplicacao/Principal.java
@@ -4,15 +4,24 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 
+import fvs.edu.dominio.Aluno;
+
 public class Principal {
 
 	public static void main(String[] args) {
-		EntityManagerFactory emf =
-				Persistence.createEntityManagerFactory
-				("topicos-fvs");
+		EntityManagerFactory emf = Persistence.createEntityManagerFactory("topicos-fvs");
 		
 		EntityManager em = emf.createEntityManager();
-
+		
+		Aluno aluno = em.find(Aluno.class, 8);
+		System.out.println(aluno);
+		
+		//em.getTransaction().begin();
+		//Aluno aluno1 = new Aluno(null, "thalis", "65465-76", "544556", 65465);
+		//em.persist(aluno1);	
+		//em.getTransaction().commit();
+		//System.out.println("Salvo com Sucesso");
 	}
 
 }
+ 
--- src/fvs/edu/dominio/Aluno.java
+++ src/fvs/edu/dominio/Aluno.java
@@ -6,20 +6,17 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 
-
 @Entity
 public class Aluno implements Serializable{
 	private static final long serialVersionUID = 1L;
 	
 	@Id
 	@GeneratedValue
-	
 	private Integer id;
 	private String nome;
 	private String cpf;
 	private String rg;
 	private Integer matricula;
-	
 	
 	
 	public Aluno(Integer id, String nome, String cpf, String rg, Integer matricula) {
@@ -32,72 +29,20 @@ public class Aluno implements Serializable{
 	}
 	
 	
-	
+
+	public Aluno() {
+		super();
+	}
+
+
+
 	@Override
 	public String toString() {
 		return "Aluno [id=" + id + ", nome=" + nome + ", cpf=" + cpf + ", rg=" + rg + ", matricula=" + matricula + "]";
 	}
+	
 
-
-
-	public Integer getId() {
-		return id;
-	}
-
-
-
-	public void setId(Integer id) {
-		this.id = id;
-	}
-
-
-
-	public String getNome() {
-		return nome;
-	}
-
-
-
-	public void setNome(String nome) {
-		this.nome = nome;
-	}
-
-
-
-	public String getCpf() {
-		return cpf;
-	}
-
-
-
-	public void setCpf(String cpf) {
-		this.cpf = cpf;
-	}
-
-
-
-	public String getRg() {
-		return rg;
-	}
-
-
-
-	public void setRg(String rg) {
-		this.rg = rg;
-	}
-
-
-
-	public Integer getMatricula() {
-		return matricula;
-	}
-
-
-
-	public void setMatricula(Integer matricula) {
-		this.matricula = matricula;
-	}
-
+	
 
 
 	@Override
@@ -111,6 +56,64 @@ public class Aluno implements Serializable{
 		result = prime * result + ((rg == null) ? 0 : rg.hashCode());
 		return result;
 	}
+	
+	
+	
+	public Integer getId() {
+		return id;
+	}
+
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+
+	public String getNome() {
+		return nome;
+	}
+
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+
+	public String getCpf() {
+		return cpf;
+	}
+
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+
+
+	public String getRg() {
+		return rg;
+	}
+
+
+	public void setRg(String rg) {
+		this.rg = rg;
+	}
+
+
+	public Integer getMatricula() {
+		return matricula;
+	}
+
+
+	public void setMatricula(Integer matricula) {
+		this.matricula = matricula;
+	}
+
+
+	public static long getSerialversionuid() {
+		return serialVersionUID;
+	}
+
+
 	@Override
 	public boolean equals(Object obj) {
 		if (this == obj)
@@ -149,5 +152,5 @@ public class Aluno implements Serializable{
 	}
 	
 	
-
+	
 }
