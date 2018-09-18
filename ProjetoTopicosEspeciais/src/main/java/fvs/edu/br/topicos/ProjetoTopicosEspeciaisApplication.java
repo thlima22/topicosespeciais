283de6f5d520a4fe9424a51a3e0832d497fd15db
unknown <thalislima009@gmail.com>
2018-09-18 20:08:50 -0300

    Implementação da camad de serviços de catgoria

--- ProjetoTopicosEspeciais/src/main/java/fvs/edu/br/topicos/ProjetoTopicosEspeciaisApplication.java
+++ ProjetoTopicosEspeciais/src/main/java/fvs/edu/br/topicos/ProjetoTopicosEspeciaisApplication.java
@@ -1,4 +1,4 @@
-tpackage fvs.edu.br.topicos;
+package fvs.edu.br.topicos;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- ProjetoTopicosEspeciais/src/main/java/fvs/edu/br/topicos/resources/CategoriaResource.java
+++ ProjetoTopicosEspeciais/src/main/java/fvs/edu/br/topicos/resources/CategoriaResource.java
@@ -1,28 +1,28 @@
 package fvs.edu.br.topicos.resources;
 
-import java.util.ArrayList;
-import java.util.List;
-
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import fvs.edu.br.topicos.domain.Categoria;
+import fvs.edu.br.topicos.services.CategoriaService;
 
 @RestController
 @RequestMapping(value="/categorias")
 public class CategoriaResource {
+	
+	@Autowired
+	private CategoriaService service;
 
-	@RequestMapping(method=RequestMethod.GET)
-	public List<Categoria> listar () {
-		Categoria cat1 = new Categoria (1, "Bombril");
-		Categoria cat2 = new Categoria (2, "detergente");
+	@RequestMapping(value="/{id}", method=RequestMethod.GET)
+	public ResponseEntity<?> listar(@PathVariable Integer id) {
+		Categoria obj = service.buscar(id);
+
 		
-		List <Categoria> lista = new ArrayList<>();
-		lista.add(cat1);
-		lista.add(cat2);
-		
-		return lista;
+		return ResponseEntity.ok().body(obj);
 	}
 	
 }
