82abb04a0a4f301b51fd1d59d61918ef27134e6b
thalis lima <thalislima009@gmail.com>
2018-11-05 20:54:53 -0200

    Implementação dos recursos e serviços

--- ProjetoTopicosEspeciais/src/main/java/fvs/edu/br/topicos/domain/Pedido.java
+++ ProjetoTopicosEspeciais/src/main/java/fvs/edu/br/topicos/domain/Pedido.java
@@ -44,4 +44,92 @@ public class Pedido implements Serializable {
 	
 	@OneToOne (cascade=CascadeType.ALL, mappedBy="pedido")
 	private Pagamento pagamento;
+	
+	public Pedido() {
+		
+	}
+
+	public Pedido(Integer id, Date instante, Cliente cliente, Endereco enderecoEntregue) {
+		super();
+		this.id = id;
+		this.instante = instante;
+		this.cliente = cliente;
+		this.enderecoEntregue = enderecoEntregue;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public Date getInstante() {
+		return instante;
+	}
+
+	public void setInstante(Date instante) {
+		this.instante = instante;
+	}
+
+	public Cliente getCliente() {
+		return cliente;
+	}
+
+	public void setCliente(Cliente cliente) {
+		this.cliente = cliente;
+	}
+
+	public Endereco getEnderecoEntregue() {
+		return enderecoEntregue;
+	}
+
+	public void setEnderecoEntregue(Endereco enderecoEntregue) {
+		this.enderecoEntregue = enderecoEntregue;
+	}
+
+	public Set<ItemPedido> getItens() {
+		return itens;
+	}
+
+	public void setItens(Set<ItemPedido> itens) {
+		this.itens = itens;
+	}  
+
+	public Pagamento getPagamento() {
+		return pagamento;
+	}
+
+	public void setPagamento(Pagamento pagamento) {
+		this.pagamento = pagamento;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Pedido other = (Pedido) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+	
+	
+	
 }
---
+++ ProjetoTopicosEspeciais/src/main/java/fvs/edu/br/topicos/repositories/EnderecoRepository.java
@@ -0,0 +1,11 @@
+package fvs.edu.br.topicos.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import fvs.edu.br.topicos.domain.Endereco;
+
+@Repository
+public interface EnderecoRepository extends JpaRepository<Endereco, Integer> {
+
+}
