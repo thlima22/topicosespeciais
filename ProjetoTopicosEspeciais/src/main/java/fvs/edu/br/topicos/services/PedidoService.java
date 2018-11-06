609d0e024e2c3ee3cf01d8231b8f7d2ddf082f3f
Thalis Lima <thalislima009@gmail.com>
2018-11-06 20:56:52 -0200

    Implementação Recurso e Serviço de Pedido

---
+++ ProjetoTopicosEspeciais/src/main/java/fvs/edu/br/topicos/services/PedidoService.java
@@ -0,0 +1,21 @@
+package fvs.edu.br.topicos.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import fvs.edu.br.topicos.domain.Pedido;
+import fvs.edu.br.topicos.repositories.PedidoRepository;
+import javassist.tools.rmi.ObjectNotFoundException;
+
+public class PedidoService {
+	@Autowired
+	private PedidoRepository repo;
+	
+	public Pedido buscar(Integer id)
+	throws ObjectNotFoundException{
+		Optional<Pedido> obj = repo.findById(id);
+		return obj.orElse(null);
+		
+	}
+}
